87b5998945d9146b8321324a08b41ca1c9ac90e6
Donovan Lange <dolange@microsoft.com>
2019-06-17 14:49:47 -0700

    Initial commit; paired down some

---
+++ README.md
@@ -0,0 +1,23 @@
+# giraffe_test
+
+A [Giraffe](https://github.com/giraffe-fsharp/Giraffe) web application, which has been created via the `dotnet new giraffe` command.
+
+## Build and test the application
+
+### Windows
+
+Run the `build.bat` script in order to restore, build and test (if you've selected to include tests) the application:
+
+```
+> ./build.bat
+```
+
+## Run the application
+
+After a successful build you can start the web application by executing the following command in your terminal:
+
+```
+dotnet run src/giraffe_test
+```
+
+After the application has started visit [http://localhost:5000](http://localhost:5000) in your preferred browser.
---
+++ src/Program.fs
@@ -0,0 +1,55 @@
+module Api
+
+open System
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Cors.Infrastructure
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.Logging
+open Microsoft.Extensions.DependencyInjection
+open Giraffe
+
+let webApp =
+    choose [
+        GET >=>
+            choose [
+                route "/" >=> text "Hello world!"
+            ]
+        setStatusCode 404 >=> text "Not Found" ]
+
+let configureApp (app : IApplicationBuilder) =
+    let errorHandler (ex : Exception) (logger : ILogger) =
+        logger.LogError(ex, "An unhandled exception has occurred while executing the request.")
+        clearResponse >=> setStatusCode 500 >=> text ex.Message
+
+    let configureCors (builder : CorsPolicyBuilder) =
+        builder.WithOrigins("http://localhost:8080")
+               .AllowAnyMethod()
+               .AllowAnyHeader()
+               |> ignore
+
+    let env = app.ApplicationServices.GetService<IHostingEnvironment>()
+    (match env.IsDevelopment() with
+    | true  -> app.UseDeveloperExceptionPage()
+    | false -> app.UseGiraffeErrorHandler errorHandler)
+        .UseCors(configureCors)
+        .UseGiraffe(webApp)
+
+let configureServices (services : IServiceCollection) =
+    services.AddCors()    |> ignore
+    services.AddGiraffe() |> ignore
+
+let configureLogging (builder : ILoggingBuilder) =
+    builder.AddFilter(fun l -> l.Equals LogLevel.Error)
+           .AddConsole()
+           .AddDebug() |> ignore
+
+[<EntryPoint>]
+let main _ =
+    WebHostBuilder()
+        .UseKestrel(fun options -> options.ListenLocalhost 4000)
+        .Configure(Action<IApplicationBuilder> configureApp)
+        .ConfigureServices(configureServices)
+        .ConfigureLogging(configureLogging)
+        .Build()
+        .Run()
+    0
